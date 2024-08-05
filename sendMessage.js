c6b3bbf688f9f37604be08c163f0262cb4bdd2bf
Nils. <81931103+utigernils@users.noreply.github.com>
2024-08-05 15:23:15 +0200

    clearing of Boxes after sent message

--- sendMessage.js
+++ sendMessage.js
@@ -3,13 +3,13 @@ contentInput = document.getElementById('content-input');
 senderInput = document.getElementById('sender-input');
 sendButton = document.getElementById('send-button');
 
-sendButton.addEventListener('click', async function() {
+async function sendMessage() {
   console.log('send button clicked');
   var title = titleInput.value;
   var content = contentInput.value;
   var sender = senderInput.value;
 
-  const MSGurl = "http://192.168.1.1:5000/create_message?title=" + title +"&content=" + content + "&sender=" + sender;
+  const MSGurl = "http://192.168.1.1:5000/create_message?title=" + title + "&content=" + content + "&sender=" + sender;
 
   try {
     const response = await fetch(MSGurl);
@@ -19,8 +19,19 @@ sendButton.addEventListener('click', async function() {
 
     const json = await response.json();
     console.log(json);
+
+    titleInput.value = '';
+    contentInput.value = '';
+    senderInput.value = '';
   } catch (error) {
     console.error(error.message);
   }
+}
 
+sendButton.addEventListener('click', sendMessage);
+
+document.addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+    sendMessage();
+  }
 });
